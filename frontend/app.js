96c20a35f8e45f92927095b5a64af36faa8d57db
Sharmaine2005 <Sharmainehannahvalenzuela.educ@gmail.com>
2025-02-25 19:56:12 +0800

    Initial commit

--- frontend/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('fetchBtn').addEventListener('click', () => {
-    fetch('http://localhost:8000/api/message')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('message').textContent = data.message;
-        })
-        .catch(error => console.error('Error:', error));
-});
